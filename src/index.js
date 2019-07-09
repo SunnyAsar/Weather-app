d8493cbd1b78b51b94fea88ef366df0acc089fe5
Sunny <ring2rings93@gmail.com>
2019-07-09 21:08:54 +0100

    handled error, and code cleanup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import Service from './lib/service'
+import DomHandler from './lib/domHandler'
 
-document.addEventListener('DOMContentLoaded', ()=> Service.getWeather())
+document.addEventListener('DOMContentLoaded', ()=> {
+  const weatherForm = document.getElementById('weather-form').addEventListener('submit', fetchWeather)
+})
+
+function fetchWeather(event){
+  event.preventDefault()
+  const city = event.target[0].value
+  Service.getWeather(city)
+  .then(data =>  {
+    DomHandler.updateUi(data)
+  })
+  .catch(err => DomHandler.handleError(err))
+}
--- src/lib/service.js
+++ src/lib/service.js
@@ -1,27 +1,26 @@
 
-  function getWeather (city='Abuja'){
+  function getWeather (city){
     const API_KEY = 'a622921a4c100b7d35d888ae68406706'
+    // const town = 'Kano'
     const url =  `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
-    fetch(url,{mode: 'cors'})
+    return fetch(url,{mode: 'cors'})
     .then((response)=> response.json())
     .then((data) => {
-      console.log(data.main.temp)
+      if(!data.main){
+        throw new Error('Enter a valid city')
+      }
+
       const temp = data.main.temp
+      const city = data.name
       const Celcius = (temp - 273.15).toFixed(1)
       const Fahrenheit = ((temp - 273.15) * (9 / 5) + 32).toFixed(1)
-      console.log({c:Celcius,f:Fahrenheit})
-
-      // const kelvinToFahrenheit = ((temp - 273.15) * (9 / 5) + 32).toFixed(1)
-      // console.log({celcius: kelvinToCelcius, fahrenheight: kelvinToFahrenheit})
-
-      // return {celcius: kelvinToCelcius, fahrenheight: kelvinToFahrenheit}
-    
+      const weather = { c:Celcius, f:Fahrenheit, city: city }
+      return weather
     })
-    .catch((err)=> console.log(err))
+    // .catch((err)=> console.log(err))
+
+
   }
 
-
-  const kelvinToCelcius = temp => (temp - 273.15).toFixed(1)
-  const kelvinToFahrenheit = temp => ((temp - 273.15) * (9 / 5) + 32).toFixed(1)
 
 export default { getWeather }
