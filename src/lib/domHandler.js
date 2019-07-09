dede5532f066740b45fbcc3a469047e81701a350
Sunny <ring2rings93@gmail.com>
2019-07-09 21:50:00 +0100

    added temprature symbols

--- src/lib/domHandler.js
+++ src/lib/domHandler.js
@@ -2,21 +2,18 @@
 function updateUi (data){
   document.getElementById('weather-report').innerHTML =  `
   <h3 class='city-head'>Weather for: ${data.city}</h3>
-  <h2>Celcious: ${data.c}</h2>
-  <h2>Fahrenheit: ${data.f}</h2>
+  <h2>Celcious: ${data.c}°c </h2>
+  <h2>Fahrenheit: ${data.f}°f</h2>
   `
   document.getElementById('error').innerHTML = ''
   document.getElementById('error').className = ''
- 
   document.getElementById('weather-form').reset()
-
 }
 
 function handleError(err){
   const error = document.getElementById('error')
   error.classList.add('alert', 'alert-danger')
   error.innerHTML = err
-
   document.getElementById('weather-report').innerHTML = ''
 }
 
